95a2f5cb3b94b250f1338d63812987db41e2244e
李传宝 <lichuanbao.buddy@gmail.com>
2016-01-28 17:26:07 +0800

    add views dir to sys path, move path define logic to appengine_config.py

--- appengine_config.py
+++ appengine_config.py
@@ -1,9 +1,10 @@
 """This file is loaded when starting a new application instance."""
 import sys
-import os.path
+import os
 import json
 
 # add `lib` subdirectory to `sys.path`, so our `main` module can load
 # third-party libraries.
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'lib'))
-
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'libs'))
+sys.path.append(os.path.join(os.path.dirname(__file__), 'views'))
--- index.py
+++ index.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 # -*- encoding:utf-8 -*-
 
-import sys,os
+#import sys,os
 
-BASE_PATH = os.path.dirname(__file__)
-LIBS_PATH = os.path.join(BASE_PATH, 'libs')
+#BASE_PATH = os.path.dirname(__file__)
+#LIBS_PATH = os.path.join(BASE_PATH, 'libs')
 ##设置环境变量
-sys.path.append(LIBS_PATH)
+#sys.path.append(LIBS_PATH)
+import appengine_config
 
 from bottle import default_app, run
 from bottle import route, mako_template as template, redirect, request, response, get, post
